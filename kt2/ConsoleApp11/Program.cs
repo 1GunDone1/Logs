8df7e424d177afe9ec08cd6e8eca2370107a2b77
1GunDone1 <128579131+1GunDone1@users.noreply.github.com>
2025-04-06 18:28:33 +1000

    Add files via upload

---
+++ kt2/ConsoleApp11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Serilog;
+using Serilog.Formatting.Json;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Настройка Serilog с JSON-форматированием
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console(new JsonFormatter())  // Логи в консоль в формате JSON
+            .WriteTo.File(
+                new JsonFormatter(),
+                "logs/log.json")
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Приложение запущено");
+
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш возраст:");
+            if (!int.TryParse(Console.ReadLine(), out int age))
+            {
+                Log.Warning("Некорректный ввод возраста");
+                Console.WriteLine("Возраст должен быть числом!");
+                return;
+            }
+
+            // Логируем полученные данные
+            Log.Information("Пользователь {@UserData}", new { Name = name, Age = age });
+            Console.WriteLine($"Данные сохранены. Имя: {name}, Возраст: {age}");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Произошла ошибка");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
